845a1ec5e67d46b691d3dfc014c77881763a986d
jzcz38 <jzcz38@bosera.com>
2020-09-14 15:58:00 +0800

    首页的编写

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,8 +3,7 @@ const app = getApp()
 
 Page({
   data: {
-    number:1,
-    timer:null
+
   },
 
   onShow: function() {
@@ -12,52 +11,11 @@ Page({
   },
 
   onLoad:function(){
-    let obj = {
-      arr:[
-        {
-          key:'dyk',
-          value:'name',
-        },
-        {
-          key:'dw',
-          value:'age',
-        },
-        {
-          key:'wf',
-          value:'addr',
-        },
-      ],
-      option:{
-        name:"1126",
-        age:'26',
-        addr:"sz"
-      }
-    }
-    let newobj={};   
-    obj.arr.forEach(item=>{
-      if(obj.option.hasOwnProperty(item.value)){
-        newobj[item.key] = obj.option[item.value]
-      }
-    })
-    console.log(newobj);
-    let arr = [];
-    for(let key in newobj){
-      arr.push([key,newobj[key]]);
-    }
-    console.log(arr)
-    let n = 1;
-    this.data.timer = setInterval(()=>{
-      n++;
-      this.setData({
-        number:n
-      })
-    },1000)
+
   },
 
   onHide(){
-    if(this.data.timer){
-      clearInterval(this.data.timer)
-    }
+
   },
 
   onGetUserInfo: function(e) {
@@ -67,10 +25,4 @@ Page({
    /**
    * 组件的方法列表
    */
-  goLogin:function(){
-    clearInterval(this.data.timer)
-    wx.navigateTo({url:'/pages/login/login'}) 
-    console.log('dyk')
-  }
-
 })
